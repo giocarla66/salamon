e5fbcd3e44f367c69b60b9ee2a00080b15e972d4
giocarla66 <giovanni.nomico@gmail.com>
2025-03-04 09:23:36 +0100

    Update distromanager.js

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -3,7 +3,7 @@ const { DistributionAPI } = require('helios-core/common')
 const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
-exports.REMOTE_DISTRO_URL = 'http://185.206.151.190/nebula/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://ariarpgmod.mtxserv.com/distribution.json'
 
 
 const api = new DistributionAPI(
